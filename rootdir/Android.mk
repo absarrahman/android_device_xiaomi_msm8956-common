d499623a957ecf3a7c8e01452bffba64f92a21b1
Ethan Chen <intervigil@gmail.com>
2018-03-17 07:34:26 +0000

    msm8956-common: Remove init.qcom.bt.sh
    * Just run the hci_qcomm_init binary instead
    
    Change-Id: If2b4b9fc738e729bd389f3f96e6843a4ca6eb558

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -50,12 +50,13 @@ LOCAL_SRC_FILES		:= etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-# Configuration scripts
-
+# Offmode charging
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.qcom.bt.sh
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_EXECUTABLES)
+LOCAL_MODULE          := chargeonlymode
+LOCAL_MODULE_OWNER    := xiaomi
+LOCAL_MODULE_PATH     := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES       := sbin/chargeonlymode
+LOCAL_MODULE_TAGS     := optional
+LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)
