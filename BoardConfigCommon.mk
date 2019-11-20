AmolAmrit/device_xiaomi_msm8956-common

msm8956-common: SEPolicy: Add temporary policies
Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>

BoardConfigCommon.mk
@@ -221,6 +221,7 @@ TARGET_LD_SHIM_LIBS := \
 # SELinux
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true

absarrahman/android_device_xiaomi_msm8956-common

BoardConfigCommon.mk
@@ -220,6 +220,7 @@ TARGET_LD_SHIM_LIBS := \
 # SELinux
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy-minimal
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true