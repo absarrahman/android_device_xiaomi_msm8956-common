d0c1a4807498192cb1761ce68e95130c90026dd8
vishalk17 <vishalkapadi95@gmail.com>
2020-04-17 03:35:12 +0000

    msm8956-common: set persist.sys.usb.config to MTP and ADB and enable debugging
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -492,6 +492,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # ANGLE
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.gfx.angle.supported=true
